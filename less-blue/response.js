df98f1a49e2dbf57062530a87a3acbd369381dbd
[neimanis] <[ernestsneimanis]>
2023-09-30 14:35:10 +0200

    second argument of response now iterable

--- less-blue/response.js
+++ less-blue/response.js
@@ -2,24 +2,21 @@ import { Cell } from "./cell.js";
 import { Entry } from "./entry.js";
 import { Winner } from "./winner.js";
 import { ComboUtils } from "./combo-utils.js";
-export class Response extends ComboUtils{
-  constructor(gameData, responseData) {
-    super(gameData)
-    this._checkArguments(gameData,responseData);
+export class Response extends ComboUtils {
+  constructor(gameData, ...responseData) {
+    super(gameData);
+   // this._checkArguments(gameData, ...responseData);
 
     this.gameData = gameData;
     this.responseData = responseData;
-    
-   
+
     this.playField = gameData.getPlayField();
     this.turnId = this.gameData.getTurnId();
- 
-   
-    this.response = {
 
+    this.response = {
       playField: this.playField,
       turnId: this.turnId,
-      
+
       row: undefined,
       col: undefined,
       id: undefined,
@@ -32,38 +29,63 @@ export class Response extends ComboUtils{
       playerWin: false,
       lessBlueWin: false,
       loserId: undefined,
-    
     };
 
     this._createResponse();
 
     return this.deepCopy(this.response);
   }
-// if less blue wins, response must have a response cell and winner
+  // if less blue wins, response must have a response cell and winner
+  // _createResponse() {
+  //   if (
+  //     typeof this.responseData.row === "number" &&
+  //     typeof this.responseData.col === "number"
+  //   ) {
+  //     this._fillInCellData(
+  //       new Cell(this.responseData.row, this.responseData.col)
+  //     );
+  //   }
+  //   if (
+  //     this.responseData.combo &&
+  //     typeof this.responseData.id !== "undefined"
+  //   ) {
+  //     this._fillInWinnerData(
+  //       new Winner(this.responseData.combo, this.responseData.id)
+  //     );
+  //   } else {
+  //     console.log("invalid data", this.gameData, this.responseData);
+  //     throw new Error("'responseData' has invalid argument");
+  //   }
+  // }
+
   _createResponse() {
-    if(typeof this.responseData.row ==="number" && typeof this.responseData.col === "number"){
-      this._fillInCellData(new Cell(this.responseData.row, this.responseData.col));
-    }
-    if(this.responseData.combo && typeof this.responseData.id !== "undefined"){
-      this._fillInWinnerData(new Winner(this.responseData.combo,this.responseData.id))
-    } else{
-      console.log("invalid data",this.gameData,this.responseData)
+    const responseData = this.responseData;
+    let success = false;
+    responseData.forEach((obj) => {
+      if (
+        typeof obj.row === "number" &&
+        typeof obj.col === "number"
+      ) {
+        this._fillInCellData( new Cell(obj.row, obj.col));
+        success = true;
+      }
+      if (obj.combo && typeof obj.id !== "undefined"
+      ) {
+        this._fillInWinnerData(new Winner(obj.combo, obj.id));
+        success = true;
+      }
+    });
+   // console.log("final response",this.response)
+    if(!success){
+      console.log("invalid data", this.gameData, this.responseData);
       throw new Error("'responseData' has invalid argument");
-    
     }
   }
 
-  // __createResponse(){
-  //   const responseData = this.responseData;
-    
-  // }
-
   _fillInCellData(cell) {
-  
     const response = this.response;
     const gameData = this.gameData;
     const entryId = gameData.getPlayField()[cell.row][cell.col];
-   
 
     response.row = cell.row;
     response.col = cell.col;
@@ -72,28 +94,27 @@ export class Response extends ComboUtils{
     response.entry = new Entry(cell.row, cell.col, entryId);
   }
   _fillInWinnerData(winner) {
-  
     const response = this.response;
     const { playerId, lessBlueId } = this.gameData.get();
-    const winnerId = winner.id
+    const winnerId = winner.id;
     const loserId = winner.id === playerId ? lessBlueId : playerId;
-   
+
     response.winner = winner;
     response.winnerId = winner.id;
     response.winningCombo = winner.combo;
     response.loserId = loserId;
     response.playerWin = winnerId === playerId;
     response.lessBlueWin = winnerId === lessBlueId;
-
   }
-
 
   _checkArguments(gameData, responseData) {
     if (!gameData) {
       throw new Error("'Response' must have argument of type 'gameData'!");
     }
-    if(!responseData){
-       throw new Error("'Response' must have argument 'responseData' of type 'Cell' or 'Winner'!");
+    if (!responseData) {
+      throw new Error(
+        "'Response' must have argument 'responseData' of type 'Cell' or 'Winner'!"
+      );
     }
   }
 }
